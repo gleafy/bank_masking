1e5209e86f20ab2bb2002206ac7c9f9975190ba5
gleafy <greenleafy@yandex.ru>
2025-04-08 03:18:11 +0400

    Добавил logger

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,19 @@
 """
 
 import json
+import logging
+import os
 from typing import Any, Dict, List
 
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("logs/utils.log", mode="w")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def read_json(file_path: str) -> List[Dict[str, Any]]:
     """
@@ -18,6 +29,11 @@ def read_json(file_path: str) -> List[Dict[str, Any]]:
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
+            logger.info(f"Успешно прочитан файл: {file_path}")
             return data if isinstance(data, list) else []
-    except (FileNotFoundError, json.JSONDecodeError):
+    except FileNotFoundError:
+        logger.error(f"Файл не найден: {file_path}")
+        return []
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON в файле: {file_path}")
         return []
